af7ed47458ed2adb8ee458ba7fb2ff796e57f7ec
Vasilije Lopicic <vasilije.lopicic2005@gmail.com>
2025-10-21 14:14:45 -0400

    moved class

--- TermProject/Customer.cpp
+++ TermProject/Customer.cpp
@@ -1,1 +1,50 @@
-//customer
+
+class Customer
+{
+	string fname, lname, address, phone, email;
+
+public:
+	void setFname(string _fname) //maybe find a way to validate the strings
+	{
+		fname = _fname;
+	}
+	void setLname(string _lname)
+	{
+		lname = _lname;
+
+	}
+	void setAddress(string _address)
+	{
+		address + _address;
+
+	}
+	void setPhone(string _phone)
+	{
+		phone = _phone;
+	}
+	void setemail(string _email)
+	{
+		email = _email;
+	}
+	string getFname() const
+	{
+		return fname;
+	}
+	string getLname() const
+	{
+		return lname;
+	}
+	string getAddress() const
+	{
+		return address;
+	}
+	string getPhone() const
+	{
+		return phone;
+	}
+	string getEmail() const
+	{
+		return email;
+	}
+	//yes
+};
--- TermProject/TermProject.cpp
+++ TermProject/TermProject.cpp
@@ -4,55 +4,6 @@
 
 using namespace std;
 
-class Customer
-{
-	string fname, lname, address, phone, email;
-
-public:
-	void setFname(string _fname) //maybe find a way to validate the strings
-	{
-		fname = _fname;
-	}
-	void setLname(string _lname)
-	{
-		lname = _lname;
-
-	}
-	void setAddress(string _address)
-	{
-		address + _address;
-
-	}
-	void setPhone(string _phone)
-	{
-		phone = _phone;
-	}
-	void setemail(string _email)
-	{
-		email = _email;
-	}
-	string getFname() const
-	{
-		return fname;
-	}
-	string getLname() const
-	{
-		return lname;
-	}
-	string getAddress() const
-	{
-		return address;
-	}
-	string getPhone() const
-	{
-		return phone;
-	}
-	string getEmail() const
-	{
-		return email;
-	}
-	//yes
-};
 
 
 
