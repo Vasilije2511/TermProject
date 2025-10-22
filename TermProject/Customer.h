7b3b91c9887b73109dcc8fc4419f030ec61effb1
vlopici <vlopici@JU56196DF>
2025-10-22 12:06:23 -0400

    customer.h

---
+++ TermProject/Customer.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef CUSTOMER
+#define CUSTOMER
+#include <string>
+using namespace std;
+class Customer
+{
+	string fname, lname, address, phone, email;
+public:
+	Customer();
+	Customer(string _fname, string _lname, string _address, string _phone, string _email);
+	void setFname(string _fname);
+	void setLname(string _lname);
+	void setAddress(string _address);
+	void setPhone(string _phone);
+	void setemail(string _email);
+	string getFname() const;
+	string getLname() const;
+	string getAddress() const;
+	string getPhone() const;
+	string getEmail() const;
+	void setAll(string _fname, string _lname, string _address, string _phone, string _email);
+	void printInfo();
+
+};
+
+#endif
