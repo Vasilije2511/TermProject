fa0c82838e9a69d5c0053ffa596bd8291535fd93
Vasilije Lopicic <vasilije.lopicic2005@gmail.com>
2025-11-04 21:32:07 -0400

    bunch in main

--- TermProject/TermProject.cpp
+++ TermProject/TermProject.cpp
@@ -6,198 +6,323 @@
 #include "SavingAccount.h"
 #include <vector>
 #include <iomanip>
+#include <cstdlib>
 using namespace std;
 
+void showNewUserMenu();
+void showExistingUserMenu();
 
 
 int main()
 {
-	vector<CheckingAccount*> checkingAccounts;
-	vector<SavingAccount*> savingAccounts;
+	vector<CheckingAccount> checkingAccounts;
+	vector<SavingAccount> savingAccounts;
+	vector<Customer> customers;
 	int choice;
 	int accountCounter = 1;
+	char userType;
 
-	cout << "1: creating a checking account\n2: create a saving account\n3: view account information\n4: Modify Account \n5: Delete an account\n6: transferring money between accounts\n7: Exit Program";
-	cout << "Select an option: ";
-	cin >> choice;
-	while (choice != 7)
+	cout << "Are you a new user? (y/n): ";
+	cin >> userType;
+	while (true)
 	{
-		switch (choice)//create a checking account
-		{
-		case 1:
-		{
-
-			string fname, lname, address, phone, email;
-			double balance, overdraftLimit;
-
-			cout << "Enter customer details:"<<endl;
-			cout << "First Name: ";
-			cin >> fname;
-			cout << "Last Name: ";
-			cin >> lname;
-			cout << "Address: ";
-			cin.ignore();
-			getline(cin, address);
-			cout << "Phone: ";
-			cin >> phone;
-			cout << "Email: ";
-			cin >> email;
-			cout << "Initial Balance: ";
-			cin >> balance;
-			cout << "Overdraft Limit: ";
-			cin >> overdraftLimit;
-
-			Customer* newCustomer = new Customer(fname, lname, address, phone, email);
-			CheckingAccount* newAccount = new CheckingAccount(accountCounter++, balance, overdraftLimit, newCustomer);
-			checkingAccounts.push_back(newAccount);
-
-			cout << "Checking account created successfully! Account ID: " << accountCounter - 1 << endl;
+		if (tolower(userType) == 'y')
+			showNewUserMenu();
+		else showExistingUserMenu();
+		cout << "Selsect an option: " << endl;
+		cin >> choice;
+		if (choice == 7)
 			break;
-		}
-		case 2: //create a saving account
+		if (tolower(userType) == 'y' && choice > 2)
+			cout << "Please create an accoutnt first." << endl;
+		switch (choice)
 		{
-
-			string fname, lname, address, phone, email;
-			double balance, interestRate;
-
-			cout << "Enter customer details:"<<endl;
-			cout << "First Name: ";
-			cin >> fname;
-			cout << "Last Name: ";
-			cin >> lname;
-			cout << "Address: ";
-			cin.ignore();
-			getline(cin, address);
-			cout << "Phone: ";
-			cin >> phone;
-			cout << "Email: ";
-			cin >> email;
-			cout << "Initial Balance: ";
-			cin >> balance;
-			cout << "Interest Rate (%): ";
-			cin >> interestRate;
-
-			Customer* newCustomer = new Customer(fname, lname, address, phone, email);
-			SavingAccount* newAccount = new SavingAccount(accountCounter++, balance, interestRate, newCustomer);
-			savingAccounts.push_back(newAccount);
-
-			cout << "Saving account created successfully! Account ID: " << accountCounter - 1 << endl;
-			break;
-		}
-
-		case 3: //view account information
+		case 1: // creating checking account
 		{
-			int accountId;
-			cout << "\nEnter account ID: ";
-			cin >> accountId;
+			if (tolower(userType) == 'n') {
+				// New user - create new customer and account
+				string fname, lname, address, phone, email;
+				double balance, overdraftLimit;
 
-			bool found = false;
-			for (int i = 0; i < checkingAccounts.size(); i++) {
-				if (checkingAccounts[i]->getID() == accountId) {
-					cout << "Checking Account Information:"<<endl;
-					cout << "Account ID: " << checkingAccounts[i]->getID() << endl;
-					cout << "Balance: $" << fixed << setprecision(2) << checkingAccounts[i]->getBalance() << endl;
-					cout << "Overdraft Limit: $" << checkingAccounts[i]->getOverDraftLimit() << endl;
-					checkingAccounts[i]->getAccountCustomer()->printInfo();
-					found = true;
-					break;
-				}
+				cout << "Enter customer details:" << endl;
+				cout << "First Name: ";
+				cin >> fname;
+				cout << "Last Name: ";
+				cin >> lname;
+				cout << "Address: ";
+				cin.ignore();
+				getline(cin, address);
+				cout << "Phone: ";
+				cin >> phone;
+				cout << "Email: ";
+				cin >> email;
+				cout << "Initial Balance: ";
+				cin >> balance;
+				cout << "Overdraft Limit: ";
+				cin >> overdraftLimit;
+
+				Customer newCustomer(fname, lname, address, phone, email);
+				customers.push_back(newCustomer);
+				CheckingAccount newAccount(accountCounter++, balance, overdraftLimit, &customers.back());//back just gets last user form customers
+				checkingAccounts.push_back(newAccount);
+
+				cout << "Checking account created successfully! Account ID: " << accountCounter - 1 << endl;
+				userType = 'n'; // Convert to existing user
 			}
+			else {
 
-			if (!found) {
-				for (int i = 0; i < savingAccounts.size(); i++) {
-					if (savingAccounts[i]->getID() == accountId) {
-						cout << "Saving Account Information:"<<endl;
-						cout << "Account ID: " << savingAccounts[i]->getID() << endl;
-						cout << "Balance: $" << fixed << setprecision(2) << savingAccounts[i]->getBalance() << endl;
-						cout << "Interest Rate: " << savingAccounts[i]->getInterestRate() << "%" << endl;
-						savingAccounts[i]->getAccountCustomer()->printInfo();
-						found = true;
+
+				Customer* existingCustomer = nullptr;
+
+				string fname, lname;
+				cout << "Enter First Name: ";
+				cin >> fname;
+				cout << "Enter Last Name: ";
+				cin >> lname;
+
+				for (int i = 0; i < customers.size(); i++)
+				{
+					if (customers[i].getFname() == fname && customers[i].getLname() == lname) {
+						existingCustomer = &customers[i];
 						break;
 					}
 				}
-			}
+				if (existingCustomer) {//so if it is nullptr still this wont execute
+					// Create additional account for existing customer
+					double balance, overdraftLimit;
+					cout << "Initial Balance for new account: ";
+					cin >> balance;
+					cout << "Overdraft Limit for new account: ";
+					cin >> overdraftLimit;
 
-			if (!found) {
-				cout << "Account not found!"<<endl;
+					CheckingAccount newAccount(accountCounter++, balance, overdraftLimit, existingCustomer);
+					checkingAccounts.push_back(newAccount);
+
+					cout << "Additional checking account created successfully! Account ID: " << accountCounter - 1 << endl;
+					cout << "Account linked to customer: " << existingCustomer->getFname() << " "
+						<< existingCustomer->getLname() << endl;
+				}
+				else {
+					cout << "Customer not found! Please try again or create a new customer profile." << endl;
+				}
 			}
 			break;
 		}
-		case 4: //Modify Account
+		case 2://create saving acc
 		{
-			int accountId;
-			cout << "Enter account ID to modify: ";
-			cin >> accountId;
+			if (tolower(userType) == 'y') {
+				string fname, lname, address, phone, email;
+				double balance, interestRate;
 
+				cout << "Enter customer details:" << endl;
+				cout << "First Name: ";
+				cin >> fname;
+				cout << "Last Name: ";
+				cin >> lname;
+				cout << "Address: ";
+				cin.ignore();
+				getline(cin, address);
+				cout << "Phone: ";
+				cin >> phone;
+				cout << "Email: ";
+				cin >> email;
+				cout << "Initial Balance: ";
+				cin >> balance;
+				cout << "Interest Rate (%): ";
+				cin >> interestRate;
+
+				Customer newCustomer(fname, lname, address, phone, email);
+				customers.push_back(newCustomer);
+				SavingAccount newAccount(accountCounter++, balance, interestRate, &customers.back());
+				savingAccounts.push_back(newAccount);
+
+				cout << "Saving account created successfully! Account ID: " << accountCounter - 1 << endl;
+				userType = 'n';
+			}
+			else {
+				string fname, lname;
+				cout << "Enter first name: ";
+				cin >> fname;
+				cout << "Enter last name: ";
+				cin >> lname;
+				Customer* existingCustomer = nullptr;
+				for (int i = 0; i < customers.size(); i++)
+				{
+					if (fname == customers[i].getFname() && lname == customers[i].getLname())
+						existingCustomer = &customers[i];
+				}
+				if (existingCustomer) {
+					double balance, interestRate;
+					cout << "Initial Balance for new account: ";
+					cin >> balance;
+					cout << "Interest Rate (%): ";
+					cin >> interestRate;
+					SavingAccount newAccount(accountCounter++, balance, interestRate, existingCustomer);
+					savingAccounts.push_back(newAccount);
+
+					cout << "Additional savings account created successfully! Account ID: " << accountCounter - 1 << endl;
+				}
+				else {
+					cout << "Customer not found! Please create a new account as a new user." << endl;
+				}
+			}
+			break;
+
+		}
+		case 3://view account information
+		{
+			string fname, lname;
+			cout << "Enter first name: ";
+			cin >> fname;
+			cout << "Enter last name: ";
+			cin >> lname;
 			bool found = false;
-			for (int i = 0; i < savingAccounts.size(); i++) {
-				if (savingAccounts[i]->getID() == accountId) {
-					string fname, lname, address, phone, email;
-					cout << "Enter new customer details: "<<endl;
-					cout << "First Name: ";
-					cin >> fname;
-					cout << "Last Name: ";
-					cin >> lname;
-					cout << "Address: ";
-					cin.ignore();
-					getline(cin, address);
-					cout << "Phone: ";
-					cin >> phone;
-					cout << "Email: ";
-					cin >> email;
-
-					savingAccounts[i]->getAccountCustomer()->setAll(fname, lname, address, phone, email);
+			cout << "Accounts for " << fname << " " << lname << ": " << endl;
+			for (int i = 0; i < checkingAccounts.size(); i++)
+			{
+				if (checkingAccounts[i].getAccountCustomer()->getFname() == fname &&
+					checkingAccounts[i].getAccountCustomer()->getLname() == lname) {
+					cout << "Checking Account Information:" << endl;
+					cout << "Account ID: " << checkingAccounts[i].getID() << endl;
+					cout << "Balance: $" << fixed << setprecision(2) << checkingAccounts[i].getBalance() << endl;
+					cout << "Overdraft Limit: $" << checkingAccounts[i].getOverDraftLimit() << endl;
+					checkingAccounts[i].getAccountCustomer()->printInfo();
 					found = true;
-					cout << "Account modified successfully!"<<endl;
-					break;
 				}
 			}
 
+			// Check savings accounts
 			for (int i = 0; i < savingAccounts.size(); i++) {
-				if (savingAccounts[i]->getID() == accountId) {
-					string fname, lname, address, phone, email;
-					cout << "Enter new customer details: "<<endl;
-					cout << "First Name: ";
-					cin >> fname;
-					cout << "Last Name: ";
-					cin >> lname;
-					cout << "Address: ";
-					cin.ignore();
-					getline(cin, address);
-					cout << "Phone: ";
-					cin >> phone;
-					cout << "Email: ";
-					cin >> email;
-
-					savingAccounts[i]->getAccountCustomer()->setAll(fname, lname, address, phone, email);
+				if (savingAccounts[i].getAccountCustomer()->getFname() == fname &&
+					savingAccounts[i].getAccountCustomer()->getLname() == lname) {
+					cout << "Saving Account Information:" << endl;
+					cout << "Account ID: " << savingAccounts[i].getID() << endl;
+					cout << "Balance: $" << fixed << setprecision(2) << savingAccounts[i].getBalance() << endl;
+					cout << "Interest Rate: " << savingAccounts[i].getInterestRate() << "%" << endl;
+					savingAccounts[i].getAccountCustomer()->printInfo();
 					found = true;
-					cout << "Account modified successfully!"<<endl;
+				}
+			}
+
+			if (!found) {
+				cout << "No accounts found for this customer." << endl;
+			}
+			break;
+		}
+		case 4://modify acc
+		{
+			string fname, lname;
+			cout << "Enter first name: ";
+			cin >> fname;
+			cout << "Enter last name: ";
+			cin >> lname;
+			bool found = false;
+			for (int i = 0; i < customers.size(); i++)
+			{
+				if (fname == customers[i].getFname() && lname == customers[i].getLname())
+				{
+					string newAddress, newPhone, newEmail;
+					cout << "Enter new address: ";
+					cin.ignore();
+					getline(cin, newAddress);
+					cout << "Enter new phone: ";
+					cin >> newPhone;
+					cout << "Enter new email: ";
+					cin >> newEmail;
+
+					customers[i].setAll(fname, lname, newAddress, newPhone, newEmail);
+					cout << "Customer information updated successfully!" << endl;
+					found = true;
 					break;
 				}
 			}
 
 			if (!found) {
-				cout << "Account not found!"<<endl;
+				cout << "Customer not found!" << endl;
 			}
 			break;
 		}
-		case 5://Delete an account
+		case 5: //delete acc
 		{
+			string fname, lname;
+			int opt;
 
-			break;
+			cout << "Enter first name: ";
+			cin >> fname;
+			cout << "Enter last name: ";
+			cin >> lname;
+			bool found = false;
+			cout << "Do you want to delete checking account(1), saving acount(2) or all of them(3)? " << endl;
+			do {
+				cout << "Enter your option (1 or 2 or 3): ";
+				cin >> opt;
+				if (opt == 1)
+				{
+					for (int i = 0; i < checkingAccounts.size(); i++)
+					{
+						if (checkingAccounts[i].getAccountCustomer()->getFname() == fname &&
+							checkingAccounts[i].getAccountCustomer()->getLname() == lname) {
+							checkingAccounts.erase(checkingAccounts.begin() + i);//this is erase function and it needs v.begin()+index
+							cout << "Checking account deleted!" << endl;
+						}
+					}
+				}
+				else if (opt == 2)
+					for (int i = 0; i < savingAccounts.size(); i++) {
+						if (savingAccounts[i].getAccountCustomer()->getFname() == fname &&
+							savingAccounts[i].getAccountCustomer()->getLname() == lname) {
+							savingAccounts.erase(savingAccounts.begin() + i);
+							cout << "Saving account deleted! " << endl;
+						}
+					}
+				else if (opt == 3)
+				{
+
+					for (int i = 0; i < checkingAccounts.size(); i++)
+					{
+						if (checkingAccounts[i].getAccountCustomer()->getFname() == fname &&
+							checkingAccounts[i].getAccountCustomer()->getLname() == lname) {
+							checkingAccounts.erase(checkingAccounts.begin() + i);
+							cout << "Checking account deleted!" << endl;
+						}
+					}
+					for (int i = 0; i < savingAccounts.size(); i++) {
+						if (savingAccounts[i].getAccountCustomer()->getFname() == fname &&
+							savingAccounts[i].getAccountCustomer()->getLname() == lname) {
+							savingAccounts.erase(savingAccounts.begin() + i);
+							cout << "Saving account deleted! " << endl;
+						}
+					}
+				}
+			} while (opt < 4 && opt>0);
 		}
-		case 6://transferring money between accounts
+		case 6: //transfer money
 		{
-
-			break;
+			string fname, lname;
+			cout << "Enter account owner's First Name: ";
+			cin >> fname;
+			cout << "Enter account owner's Last Name: ";
+			cin >> lname;
 		}
-
+		return 0;
 		}
-
-		cout << "1: creating a checking account\n2: create a saving account\n3: view account information\n4: Modify Account \n5: Delete an account\n6: transferring money between accounts\n7: Exit Program";
-		cout << "Select an option: ";
-		cin >> choice;
 	}
+}
 
-	return 0;
+void showNewUserMenu() {
+	cout << endl<<"=== New User Menu ==="<<endl;
+	cout << "1: Create a checking account"<<endl;
+	cout << "2: Create a saving account"<<endl;
+	cout << "7: Exit Program"<<endl;
+}
+
+void showExistingUserMenu() {
+	cout << endl<<"=== Banking System Menu ==="<<endl;
+	cout << "1: Create a checking account\n";
+	cout << "2: Create a saving account\n";
+	cout << "3: View account information\n";
+	cout << "4: Modify Account\n";
+	cout << "5: Delete an account\n";
+	cout << "6: Transfer money between accounts\n";
+	cout << "7: Exit Program\n";
 }
